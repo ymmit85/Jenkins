ccbdf5a5c6e9ce03edc421257d0640a934494200
Tim <tim@ymmit.net>
2018-06-23 21:36:09 +0800

    new

--- jenkins_test.ps1
+++ jenkins_test.ps1
@@ -11,5 +11,5 @@ write-host $env:service
 # Using the environment variables exposed by the Jenkins job 
 
 $svc = Get-Service -Name $service
-Start-Service -Name alg
+Start-Service -Name AppReadiness
 Set-Content -Path "C:\temp\jenkins_1.txt" -Value $svc
---
+++ svc.ps1
@@ -0,0 +1,2 @@
+$svc = Get-Service
+Set-Content -Value $svc -Path c:\temp\output.txt    
