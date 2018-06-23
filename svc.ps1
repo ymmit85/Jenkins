3c51c329c0f87b7108f3a6e004bc668e1d968368
Tim <tim@ymmit.net>
2018-06-23 21:53:38 +0800

    update

--- svc.ps1
+++ svc.ps1
@@ -1,5 +1,11 @@
-$svc = Get-Service -Name wuauserv
-Set-Content -Value $svc -Path c:\temp\output.txt    
+try {
+$svc = Get-Service -Name wuauserv; Set-Content -Value $svc -Path c:\temp\output.txt    
+}
+catch {
+    write-host $error[0]
+    throw $LASTEXITCODE
+}
+
 #Start-Service wuauserv
 #$svc = Get-Service -Name wuauserv
 #Set-Content -Value $svc -Path c:\temp\output1.txt    
