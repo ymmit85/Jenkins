ebcf0889915fa19f3d9db8652669e7dbb1441372
Tim <tim@ymmit.net>
2018-06-23 21:40:45 +0800

    new

--- svc.ps1
+++ svc.ps1
@@ -1,2 +1,5 @@
-$svc = Get-Service
+$svc = Get-Service -Name wuauserv
 Set-Content -Value $svc -Path c:\temp\output.txt    
+Start-Service wuauserv
+Set-Content -Value $svc -Path c:\temp\output1.txt    
+ 
